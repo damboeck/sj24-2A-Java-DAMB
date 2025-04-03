f655c8b1194be0f3dc0d1ed235fabece125eaf8d
Werner Damboeck <werner.damboeck@htlstp.ac.at>
2025-04-03 11:59:43 +0200

    Listener

--- src/at/ac/htlstp/et/sj24/k2a/graphisch/MyFrame.java
+++ src/at/ac/htlstp/et/sj24/k2a/graphisch/MyFrame.java
@@ -1,23 +1,38 @@
 package at.ac.htlstp.et.sj24.k2a.graphisch;
 
 import java.awt.*;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.event.*;
 
 /**
  * Basisklasse für eine Fenster in der AWT
  */
-public class MyFrame extends Frame implements WindowListener {
+public class MyFrame
+        extends Frame
+        implements WindowListener,
+                   ComponentListener,
+                   KeyListener,
+                   MouseListener,
+                   MouseMotionListener,
+                   MouseWheelListener    {
 
     public MyFrame(String title, int width, int height) {
         setTitle(title);
         setSize(width, height);
         addWindowListener(this);
+        addComponentListener(this);
+        addKeyListener(this);
+        addMouseListener(this);
+        addMouseMotionListener(this);
+        addMouseWheelListener(this);
     }
 
     @Override
     public void windowClosing(WindowEvent e) {
         System.exit(0);
+    }
+    @Override
+    public void componentResized(ComponentEvent e) {
+        repaint();
     }
 
     @Override public void windowOpened(WindowEvent e) {
@@ -38,4 +53,45 @@ public class MyFrame extends Frame implements WindowListener {
     @Override public void windowDeactivated(WindowEvent e) {
 
     }
+    @Override public void componentMoved(ComponentEvent e) { }
+    @Override public void componentShown(ComponentEvent e) {
+
+    }
+    @Override public void componentHidden(ComponentEvent e) {
+
+    }
+    @Override public void keyTyped(KeyEvent e) {
+
+    }
+    @Override public void keyPressed(KeyEvent e) {
+
+    }
+    @Override public void keyReleased(KeyEvent e) {
+
+    }
+    @Override public void mouseClicked(MouseEvent e) {
+
+    }
+    @Override public void mousePressed(MouseEvent e) {
+
+    }
+    @Override public void mouseReleased(MouseEvent e) {
+
+    }
+    @Override public void mouseEntered(MouseEvent e) {
+
+    }
+    @Override public void mouseExited(MouseEvent e) {
+
+    }
+    @Override public void mouseDragged(MouseEvent e) {
+
+    }
+    @Override public void mouseMoved(MouseEvent e) {
+
+    }
+    @Override public void mouseWheelMoved(MouseWheelEvent e) {
+
+    }
+
 }
---
+++ src/at/ac/htlstp/et/sj24/k2a/graphisch/RechteckMusterSMUE.java
@@ -0,0 +1,57 @@
+package at.ac.htlstp.et.sj24.k2a.graphisch;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class RechteckMusterSMUE extends MyFrame {
+
+    public static final int HEAD = 25;
+    public static final int FOOT = 5;
+
+    private Color farbe;
+
+    public RechteckMusterSMUE(String title, int width, int height) {
+        super(title, width, height);
+        farbe = Color.blue;
+    }
+
+    public static void main(String[] args) {
+        RechteckMusterSMUE frame = new RechteckMusterSMUE("Rechteck Muster SMUE", 800, 600);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void paint(Graphics g){
+        g.setColor(farbe);
+        int w = getWidth();
+        int h = getHeight();
+        /*g.drawLine(0,0,w/4,h/4);
+        g.drawLine(w*3/4,h/4,w,0);
+        g.drawLine(0,h,w/4,h*3/4);
+        g.drawLine(w*3/4,h*3/4,w,h);
+        g.drawRect(w/4,h/4,w/2,h/2);*/
+        h-=HEAD+FOOT;
+        g.drawLine(0,HEAD+0,w/4,HEAD+h/4);
+        g.drawLine(w*3/4,HEAD+h/4,w,HEAD+0);
+        g.drawLine(0,HEAD+h,w/4,HEAD+h*3/4);
+        g.drawLine(w*3/4,HEAD+h*3/4,w,HEAD+h);
+        g.drawRect(w/4,HEAD+h/4,w/2,h/2);
+    }
+
+    @Override public void keyPressed(KeyEvent e) {
+        boolean doRepaint=true;
+        switch(e.getKeyChar()) {
+            case 'r' : farbe = Color.red; break;
+            case 'g' : farbe = Color.green; break;
+            case 'b' : farbe = Color.blue; break;
+            case 'y' : farbe = Color.yellow; break;
+            case 'm' : farbe = Color.magenta; break;
+            case 'o' : farbe = Color.orange; break;
+            case 'w' : farbe = Color.white; break;
+            case 'k' : farbe = Color.black; break;
+            default : doRepaint = false;
+        }
+        if (doRepaint) repaint();
+    }
+
+}
